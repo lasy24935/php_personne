9a43b131d9c0a47c059454578ed98e1167474e2b
lasy24935 <lasy24935@gmail.com>
2020-04-22 22:12:07 +0000

    ajout link form

--- header.php
+++ header.php
@@ -42,6 +42,10 @@
                </li>
             
            </ul>
-          
+          <form class="form-inline my-2 my-lg-0">
+               <input class="form-control mr-sm-2" type="text" placeholder="Recherche">
+               <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Recherche</button>
+           </form>
        </div>
    </nav>
+
